af941afbe7a546e8e6747b264686234c6c419787
Joshua Tan <joshuatan0806@gmail.com>
2022-01-28 23:15:45 +1030

    initial commit

---
+++ Waifu Game/Assets/Character.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Character", menuName = "Character", order = 0)]
+public class Character : ScriptableObject
+{
+ 
+}
