b8029422d934f994d46c35980e1dd9cb8dd078aa
zachary newman <zach.newman981@yahoo.com.au>
2022-01-29 11:18:09 +1030

    Gatcha added

--- Waifu Game/Assets/Scripts/Gatcha/GatchaUI.cs
+++ Waifu Game/Assets/Scripts/Gatcha/GatchaUI.cs
@@ -25,18 +25,27 @@ public class GatchaUI : MonoBehaviour
 
     [Space]
 
+    [SerializeField] Button backButton;
+
+    [Space]
+
     [SerializeField] VisualCard unlockPreview;
 
     private void Start()
     {
         rollGatchaButton.onClick.AddListener(RollGatcha);
 
-        unlockPreview.parent.SetActive(false);
+        backButton.onClick.AddListener(() => 
+        {
+            gameObject.SetActive(false);
+        });
+
+        HideCardPreview();
     }
 
     void RollGatcha()
     {
-        unlockPreview.parent.SetActive(false);
+        HideCardPreview();
 
         buttonParticles?.Play();
 
@@ -56,7 +65,30 @@ public class GatchaUI : MonoBehaviour
 
         Character unlocked = GatchaController.instance?.DoGatcha();
 
+        if (!unlocked)
+            yield break;
+
         unlockPreview.parent.SetActive(true);
-        //unlockPreview.splashArt = unlocked
+        unlockPreview.splashArt.sprite = unlocked?.SplashArt;
+        unlockPreview.titleTxt.SetText(unlocked.Name);
+        unlockPreview.backGround.sprite = unlocked?.SplashBackground;
+
+        float elapsed = 0;
+        float speed = .5f;
+
+        while(elapsed < 2)
+        {
+            unlockPreview.parent.transform.localScale += new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, speed * Time.deltaTime);
+
+            elapsed += Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void HideCardPreview()
+    {
+        unlockPreview.parent.SetActive(false);
+        unlockPreview.parent.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
     }
 }
