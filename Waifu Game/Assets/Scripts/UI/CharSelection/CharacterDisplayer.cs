6161377af9ec426cec4164cfd56201f63e3598bd
zachary newman <zach.newman981@yahoo.com.au>
2022-02-05 09:35:51 +1030

    Can now sort characters by stuff

--- Waifu Game/Assets/Scripts/UI/CharSelection/CharacterDisplayer.cs
+++ Waifu Game/Assets/Scripts/UI/CharSelection/CharacterDisplayer.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterDisplayer : MonoBehaviour
 {
+    enum SortType
+    {
+        none = 0,
+        alphabetical = 1,
+        rarity = 2,
+        level = 3,
+    }
+
     public Transform cardHolder;
     [SerializeField] PreviewCard cardPreviewPrefab;
 
     [Space]
     [SerializeField] Button backButton;
 
+    [Header("Filters")]
+
+    [SerializeField] TMP_Dropdown sortByDropdown;
+    SortType sortType = SortType.none;
+
     private void Start()
     {
         backButton.onClick.AddListener(() => {
             MainViewUI.instance?.SwitchView(gameObject, MainViewUI.instance.gameObject);
             Depopulate(); });
+
+        sortByDropdown.onValueChanged.AddListener(delegate 
+        {
+            sortType = (SortType)sortByDropdown.value;
+
+            PopulateCards();
+        });
 
         PopulateCards();
     }
@@ -32,20 +54,35 @@ public class CharacterDisplayer : MonoBehaviour
 
         Depopulate();
 
-        foreach (Character c in GameManager.instance.everyCharacter)
+        //Create a list from everyCharacter that only has skillLevel > 0
+        List<Character> unlockedCharacters = GameManager.instance.everyCharacter.Where(e => e.SkillLevel > 0).ToList();
+
+        //Sort the list by what we currently want to sort by
+        switch (sortType)
         {
-            if (c.SkillLevel > 0)
+            case SortType.alphabetical:
+                unlockedCharacters = unlockedCharacters.OrderBy(e => e.Name).ThenBy(e => e.CardRarity).ToList();
+                break;
+            case SortType.level:
+                unlockedCharacters = unlockedCharacters.OrderBy(e => e.Level).ThenBy(e => e.Name).ToList();
+                break;
+            case SortType.rarity:
+                unlockedCharacters = unlockedCharacters.OrderBy(e => e.CardRarity).ThenBy(e => e.Name).ToList();
+                break;
+        }
+
+
+        foreach (Character c in unlockedCharacters)
+        {
+            //We have this card
+            PreviewCard card = Instantiate(cardPreviewPrefab, cardHolder);
+            card.SetCard(c.SplashBackground, c.SplashArt, c.Name, c.CardRarity, $"Skill:{c.SkillLevel}", c.Level.ToString());
+
+            Button b = card.GetComponent<Button>();
+
+            if (b)
             {
-                //We have this card
-                PreviewCard card = Instantiate(cardPreviewPrefab, cardHolder);
-                card.SetCard(c.SplashBackground, c.SplashArt, c.Name, c.CardRarity, $"Skill:{c.SkillLevel}", c.Level.ToString());
-
-                Button b = card.GetComponent<Button>();
-
-                if (b)
-                {
-                    b.onClick.AddListener(() => CharSelector.instance.AddToSlot(c));
-                }
+                b.onClick.AddListener(() => CharSelector.instance.AddToSlot(c));
             }
         }
     }
